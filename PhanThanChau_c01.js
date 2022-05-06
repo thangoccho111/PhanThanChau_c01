ccac0a1d5e38748f446ea5d49fe44b898a2fb84e
thangoccho111 <62010994+thangoccho111@users.noreply.github.com>
2022-05-06 20:38:55 +0700

    Fix

--- PhanThanChau_c01.js
+++ PhanThanChau_c01.js
@@ -3,42 +3,48 @@ console.log('Ba Mẹ- "Cám ơn"');
 //Bài 2-3
 const a = 3;
 const b = 2;
-console.log(a / b);
-console.log(a + b);
+console.log("a / b =", a / b);
+console.log("a + b =", a + b);
 //Bài 4
 const F = 32;
 const C = (5 / 9) * (F - 32);
-console.log(C);
+console.log("Độ C là:", C);
 // Bài 5-6
 const x = 2;
 const y = 4;
-console.log(x ** 2 + y ** 2);
-console.log(Math.pow(x + y, 2));
+console.log("Tổng bình phương của 2 số x và y là:", x ** 2 + y ** 2);
+console.log("Bình phương của tổng 2 số x và y là:", Math.pow(x + y, 2));
 // Bài 7
 const a7 = 2;
 const b7 = 4;
 const c7 = 16;
-console.log((a7 ** 2 * b7 ** 2) / c7);
+console.log("Kết quả là:", (a7 ** 2 * b7 ** 2) / c7);
 //Bài 8
 const Toan = 1;
 const Ly = 22;
 const Sinh = 3;
 const Anh = 4;
-console.log((Toan + Ly + Sinh + Anh) / 4);
+const Tong = (Toan + Ly + Sinh + Anh) / 4;
+console.log("Trung bình của 4 môn là:", Tong);
 //Bài 9
 const a9 = 3;
 const b9 = 0;
-console.log(a9 / b9);
+console.log("Kết quả phép tínha/b là:", a9 / b9);
 // Bài 10
 const a10 = 3;
 const b10 = -1;
 const c10 = -2;
-console.log((-b10 + Math.sqrt(Math.pow(b10, 2) - 4 * a10 * c10)) / (2 * a10));
+console.log(
+  "Kết quả là:",
+  (-b10 + Math.sqrt(Math.pow(b10, 2) - 4 * a10 * c10)) / (2 * a10)
+);
 //Bài 11
 const x11 = 4;
 const y11 = 2;
-const S = (Math.sqrt(x11) + Math.abs(x11)) / Math.sqrt(Math.pow(x11, y11));
+const S =
+  ("Kết quả là:", Math.sqrt(x11) + Math.abs(x11)) /
+  Math.sqrt(Math.pow(x11, y11));
 console.log(S);
 // Bài 12
 const data = [1, 3, 7, 2, 4];
-console.log(data[data.length - 1]);
+console.log("Phần tử cuối cùng (dùng index):", data[data.length - 1]);

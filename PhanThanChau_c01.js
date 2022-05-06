b176fb130910fb2a652fb35ad34e3d708d47f9ae
thangoccho111 <62010994+thangoccho111@users.noreply.github.com>
2022-05-06 18:47:09 +0700

    C01

---
+++ PhanThanChau_c01.js
@@ -0,0 +1,44 @@
+//Bài 1
+console.log('Ba Mẹ- "Cám ơn"');
+//Bài 2-3
+const a = 3;
+const b = 2;
+console.log(a / b);
+console.log(a + b);
+//Bài 4
+const F = 32;
+const C = (5 / 9) * (F - 32);
+console.log(C);
+// Bài 5-6
+const x = 2;
+const y = 4;
+console.log(x ** 2 + y ** 2);
+console.log(Math.pow(x + y, 2));
+// Bài 7
+const a7 = 2;
+const b7 = 4;
+const c7 = 16;
+console.log((a7 ** 2 * b7 ** 2) / c7);
+//Bài 8
+const Toan = 1;
+const Ly = 22;
+const Sinh = 3;
+const Anh = 4;
+console.log((Toan + Ly + Sinh + Anh) / 4);
+//Bài 9
+const a9 = 3;
+const b9 = 0;
+console.log(a9 / b9);
+// Bài 10
+const a10 = 3;
+const b10 = -1;
+const c10 = -2;
+console.log((-b10 + Math.sqrt(b10 ** 2 - 4 * a10 * c10)) / (2 * a10));
+//Bài 11
+const x11 = 4;
+const y11 = 2;
+const S = (Math.sqrt(x11) + Math.abs(x11)) / Math.sqrt(Math.pow(x11, y11));
+console.log(S);
+// Bài 12
+const data = [1, 3, 7, 2, 5];
+console.log(data.length);
